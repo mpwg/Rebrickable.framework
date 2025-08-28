970a24ba00051fd6036e4437683796e4ab21afb0
Matthias Wallner-Géhri <68284225+mpwg@users.noreply.github.com>
2025-08-28 17:36:48 +0200

    Initial Commit

---
+++ Rebrickable.framework/Rebrickable_framework.swift
@@ -0,0 +1,9 @@
+//
+//  Rebrickable_framework.swift
+//  Rebrickable.framework
+//
+//  Created by Matthias Wallner-Géhri on 28.08.25.
+//
+
+import Foundation
+
---
+++ Rebrickable.frameworkTests/Rebrickable_frameworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  Rebrickable_frameworkTests.swift
+//  Rebrickable.frameworkTests
+//
+//  Created by Matthias Wallner-Géhri on 28.08.25.
+//
+
+import Testing
+@testable import Rebrickable_framework
+
+struct Rebrickable_frameworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
