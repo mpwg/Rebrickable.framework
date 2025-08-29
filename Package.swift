ae0b76ab9c8abc524810f06cf3040918a62ae035
Matthias Wallner-Géhri <68284225+mpwg@users.noreply.github.com>
2025-08-29 19:23:43 +0200

    swift 6.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Rebrickable.package",
     platforms: [
-        .macOS(.v26), .iOS(.v26), .tvOS(.v26), .watchOS(.v26)
+        .macOS(.v15), .iOS(.v18), .tvOS(.v18), .watchOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
