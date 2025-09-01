efdbee0e7aa33ec9323727edfe38febeda321210
Matthias Wallner-Géhri <68284225+mpwg@users.noreply.github.com>
2025-09-01 12:27:35 +0200

    remove old tests

--- Package.swift
+++ Package.swift
@@ -15,10 +15,6 @@ let package = Package(
             targets: ["Rebrickable.package"]
         ),
     ],
-    dependencies: [
-        // Swifter is used in tests
-        .package(url: "https://github.com/httpswift/swifter.git", from: "1.5.0")
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
@@ -29,7 +25,6 @@ let package = Package(
             name: "Rebrickable.packageTests",
             dependencies: [
                 "Rebrickable.package",
-                .product(name: "Swifter", package: "swifter")
             ]
         ),
     ]
